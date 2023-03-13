974060d13ff5cd631e75b8c3c7eb8aa20e8de467
omin29 <47471413+omin29@users.noreply.github.com>
2023-03-13 14:32:16 +0200

    Add files via upload

---
+++ VisualStudio2008-WinForms/src/Model/EllipseShape.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace Draw.src.Model
+{
+    [Serializable]
+    public class EllipseShape : Shape
+    {
+		#region Constructor
+
+		public EllipseShape(RectangleF rect) : base(rect)
+		{
+		}
+
+		public EllipseShape(RectangleShape rectangle) : base(rectangle)
+		{
+		}
+
+		#endregion
+
+		public override bool Contains(PointF point)
+		{
+			//You gotta use the LAAG formula yourself to determine if the point is inside the ellipse. Override this.
+			if (base.Contains(point))
+				return true;
+			else
+				return false;
+
+			//doesnt work
+			/*float ellipseFormula = ((point.X / Width) * (point.X / Width)) + ((point.Y / Height) * (point.Y / Height)) - 1;
+
+			if(ellipseFormula < 0)
+            {
+				return true;
+            }
+
+			return false;*/
+		}
+
+		/// <summary>
+		/// Частта, визуализираща конкретния примитив.
+		/// </summary>
+		public override void DrawSelf(Graphics grfx)
+		{
+			base.DrawSelf(grfx);
+
+			grfx.FillEllipse(new SolidBrush(FillColor), Rectangle.X, Rectangle.Y, Rectangle.Width, Rectangle.Height);
+			grfx.DrawEllipse(new Pen(StrokeColor), Rectangle.X, Rectangle.Y, Rectangle.Width, Rectangle.Height);
+
+		}
+	}
+}
---
+++ VisualStudio2008-WinForms/src/Model/GroupShape.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace Draw.src.Model
+{
+    public class GroupShape : Shape
+    {
+		#region Constructor
+
+		public GroupShape(RectangleF rect) : base(rect)
+		{
+		}
+
+		public GroupShape(RectangleShape rectangle) : base(rectangle)
+		{
+		}
+
+		#endregion
+
+		public List<Shape> SubShape { get; set; } = new List<Shape>();
+
+		public override bool Contains(PointF point)
+		{
+			//for every item in SubShape
+			//check if point in item
+
+			return false;
+		}
+
+		/// <summary>
+		/// Частта, визуализираща конкретния примитив.
+		/// </summary>
+		public override void DrawSelf(Graphics grfx)
+		{
+			//for every item in SubShape
+			//Visualize
+		}
+	}
+}
